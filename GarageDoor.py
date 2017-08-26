bee68a292afcd9e121331d529b114e15786f1af8
patrick conroy <patrick@conroy-family.net>
2017-08-26 13:29:50 -0600

    still messing w/ asJson

--- GarageDoor.py
+++ GarageDoor.py
@@ -62,9 +62,9 @@ class GarageDoor(object):
         data = {}
         data['door_id'] = self.door_id
         data['state'] = self.state
-        data['state_datetime'] = self.state_datetime.replace(microsecond=0).isoformat()
-        data['last_command'] = self.last_command.replace(microsecond=0).isoformat()
-        data['last_command_datetime'] = self.last_command_datetime
+        data['state_datetime'] = str(self.state_datetime)
+        data['last_command'] = self.last_command
+        data['last_command_datetime'] = str(self.last_command_datetime)
         print data
         print json.dumps(data)
         return json.dumps(data)
